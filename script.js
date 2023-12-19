57ba3b289c1da22efd0f85160b40c4d479db0b1c
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-19 12:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	let first_word="";
+	let n=s.length;
+	for(let i=0;i<n;i++){
+		if(s[i]=" "){
+			return;
+		}else{
+			first_word+=s[i];
+		}
+	}
+	return first_word;
 }
 
 // Do not change the code below
